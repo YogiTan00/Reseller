e3acaec0319c1422ba12e1411ab52da186fae21f
YogiTan00 <yogiwijaya00@gmail.com>
2024-08-13 23:20:32 +0700

    Feat: Change Database Without Make New Migration

--- config/database/mysql/migrations/20240604151105_create_products.up.sql
+++ config/database/mysql/migrations/20240604151105_create_products.up.sql
@@ -2,7 +2,6 @@ CREATE TABLE products (
     id              VARCHAR(225) NOT NULL UNIQUE,
     name            VARCHAR(255),
     type_size       VARCHAR(255),
-    market_type     VARCHAR(255),
     image           VARCHAR(255),
     default_price   INT,
     price           INT,
