303b47397051ed68ec0249521aa2f808fa594d0c
YogiTan00 <yogiwijaya00@gmail.com>
2024-08-15 00:42:50 +0700

    Feat: Change Database Transaction And Add Port Transaction

--- config/config.go
+++ config/config.go
@@ -7,49 +7,52 @@ import (
 )
 
 const (
-	Port        = "port"
-	PortProduct = "port_product"
-	Domain      = "domain"
-	Debug       = "debug"
-	Address     = "address"
-	DbHost      = "db_host"
-	DbPort      = "db_port"
-	DbUser      = "db_user"
-	DbPass      = "db_pass"
-	DbName      = "db_name"
-	Migration   = "migration"
-	PathLogs    = "path_logs"
+	Port            = "port"
+	PortProduct     = "port_product"
+	PortTransaction = "port_transaction"
+	Domain          = "domain"
+	Debug           = "debug"
+	Address         = "address"
+	DbHost          = "db_host"
+	DbPort          = "db_port"
+	DbUser          = "db_user"
+	DbPass          = "db_pass"
+	DbName          = "db_name"
+	Migration       = "migration"
+	PathLogs        = "path_logs"
 )
 
 type Config struct {
-	Port        string
-	PortProduct string
-	Domain      string
-	Debug       string
-	Address     string
-	DbHost      string
-	DbPort      string
-	DbUser      string
-	DbPass      string
-	DbName      string
-	Migration   string
-	PathLogs    string
+	Port            string
+	PortProduct     string
+	PortTransaction string
+	Domain          string
+	Debug           string
+	Address         string
+	DbHost          string
+	DbPort          string
+	DbUser          string
+	DbPass          string
+	DbName          string
+	Migration       string
+	PathLogs        string
 }
 
 func NewConfig() *Config {
 	cfg := &Config{
-		Port:        utils.NewEnv(Port),
-		PortProduct: utils.NewEnv(PortProduct),
-		Domain:      utils.NewEnv(Domain),
-		Debug:       utils.NewEnv(Debug),
-		Address:     utils.NewEnv(Address),
-		DbHost:      utils.NewEnv(DbHost),
-		DbPort:      utils.NewEnv(DbPort),
-		DbUser:      utils.NewEnv(DbUser),
-		DbPass:      utils.NewEnv(DbPass),
-		DbName:      utils.NewEnv(DbName),
-		Migration:   utils.NewEnv(Migration),
-		PathLogs:    utils.NewEnvDefault(PathLogs, os.TempDir()),
+		Port:            utils.NewEnv(Port),
+		PortProduct:     utils.NewEnv(PortProduct),
+		PortTransaction: utils.NewEnv(PortTransaction),
+		Domain:          utils.NewEnv(Domain),
+		Debug:           utils.NewEnv(Debug),
+		Address:         utils.NewEnv(Address),
+		DbHost:          utils.NewEnv(DbHost),
+		DbPort:          utils.NewEnv(DbPort),
+		DbUser:          utils.NewEnv(DbUser),
+		DbPass:          utils.NewEnv(DbPass),
+		DbName:          utils.NewEnv(DbName),
+		Migration:       utils.NewEnv(Migration),
+		PathLogs:        utils.NewEnvDefault(PathLogs, os.TempDir()),
 	}
 	err := cfg.Validate()
 	if err != nil {
--- config/database/mysql/migrations/20240604160437_create_transactions.up.sql
+++ config/database/mysql/migrations/20240604160437_create_transactions.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE transactions (
     id                    VARCHAR(225) NOT NULL UNIQUE,
     id_name               VARCHAR(255),
-    return                BOOLEAN,
-    sales_date            VARCHAR(255),
+    return_trans                BOOLEAN,
+    sales_date            DATETIME DEFAULT NULL,
     unit                  INT,
     description               VARCHAR(255),
     created_at            DATETIME DEFAULT NULL,
