4963780d3ec25030596dbd1352cd635bba75c4c3
YogiTan00 <yogiwijaya00@gmail.com>
2024-10-11 18:17:54 +0700

    Fix: Fixing UI Transaction

--- services/html/js/main.js
+++ services/html/js/main.js
@@ -10,3 +10,24 @@ if (imageInput) {
         document.getElementById('file-name').textContent = fileName;
     });
 }
+
+// Get the current date in yyyy-mm-dd format
+const today = new Date().toISOString().split('T')[0];
+    document.getElementById('dateNow').value = today;
+
+// Get the input and buttons
+const unitInput = document.getElementById('unit');
+const increaseBtn = document.getElementById('increaseUnit');
+const decreaseBtn = document.getElementById('decreaseUnit');
+
+// Increase unit value
+increaseBtn.addEventListener('click', function() {
+    unitInput.value = parseInt(unitInput.value) + 1;
+});
+
+// Decrease unit value, ensuring it doesn't go below 0
+decreaseBtn.addEventListener('click', function() {
+    if (unitInput.value > 0) {
+        unitInput.value = parseInt(unitInput.value) - 1;
+    }
+});
